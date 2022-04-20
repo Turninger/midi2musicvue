707852581eb42f867a5943c89d07e2da0a69afa6
Turninger <1025763047@qq.com>
2022-04-20 21:55:47 +0800

    Initial commit

---
+++ src/views/User.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>userId</td>
+                <td>userName</td>
+                <td>password</td>
+            </tr>
+            <tr>
+                <td>{{users[0].userid}}</td>
+                <td>{{users[0].username}}</td>
+                <td>{{users[0].password}}</td>
+            </tr>
+        </table>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data(){
+            return{msg:"Hello csy520",
+            users:[
+                {
+                    userid:10001,
+                    username:'csyyy',
+                    password:'xwy521'
+                },
+                {
+                    userid:10001,
+                    username:'csyyy',
+                    password:'xwy521'
+                }
+            ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
